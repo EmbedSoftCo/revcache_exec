e36965e131e77810e5b9711a485bc93674109a07
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-03-11 08:44:12 +0100

    feat: added code to test Serial communication with MCU

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
 
@@ -5,6 +8,22 @@ import 'serial_port_page.dart';
 import 'mappage.dart';
 
 void main() {
+  var sp_conf = SerialPortConfig();
+  sp_conf.baudRate = 115200;
+  sp_conf.bits = 8;
+
+  var port = SerialPort.availablePorts;
+  print(port);
+  if (port.isNotEmpty) {
+    var p = SerialPort(port[0]);
+    p.openReadWrite();
+   Uint8List bytes = utf8.encode('H'); 
+    p.write(bytes ,timeout: 5000);
+    var read = p.read(bytes.length, timeout: 5000);
+    print(read);
+    p.close();
+  }
+  
   runApp(const MyApp());
 }
 
